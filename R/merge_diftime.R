a3b24186e017831ccb17be65b5e620543bbfd8cb
Billy Mueller <william.mueller@nih.gov>
2022-02-14 12:47:03 -0500

    +(docs merge_diftime)

--- R/merge_diftime.R
+++ R/merge_diftime.R
@@ -3,6 +3,23 @@
 #' Functions that merges two dataframes that have measurements at different ages.
 #' It matches the closest measurement in data2 before, after, or before or after the
 #' measurement in data1. If a threshold is specified
+#' @param data1 a dataframe that has the ids and ages of measurement that will serve
+#' as a reference for matching with data2
+#' @param data2 a dataframe that has the ids and ages of measurement we would like to
+#' match with the reference id's and dates and data1
+#' @param id either a character string, or a character vector of length 2. If the id variable
+#' in data1 and data2 have the same name, id is a character string. If the id variable of
+#' data1 and data2 are different, then id is a character vector, with the first value being the
+#' id variable of data1 and the seoncd being the id variable for data2.
+#' @param age either a character string, or a character vector of length 2. If the age variable
+#' in data1 and data2 have the same name, age is a character string. If the age variable of
+#' data1 and data2 are different, then age is a character vector, with the first value being the
+#' age variable of data1 and the seoncd being the age variable for data2.
+#' @param threshold a numeric value that provides the difference in time between measurements
+#' allowed. The numeric should be in the same units of age.
+#' @param vars optional character vector that provides the variable names for which this should
+#' be applied over. If specified, for each variable specified by var, only non-na value will be considered
+#' when merging with the closest measurment date. See
 
 merge_diftime <- function(data1, data2, id, age, threshold = Inf, vars = NULL, where = "both", suffixes = c(".1",".2")){
   data1 <- gluc
