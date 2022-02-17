8d1e2f354f287116943e0b5c2373d71094c3ac28
William Mueller <wfmueller29@gmail.com>
2022-02-17 00:05:08 -0500

    +(impute vignette) e(df => data in impute_df) e(package title)

--- R/impute_mf.R
+++ R/impute_mf.R
@@ -3,16 +3,18 @@
 #' This function will allow the user to specify factor variables and variables they do not want
 #' in the imputation. It will then create a data matrix to impute using missForest. It will add drop columns
 #' back after imputation and relabel the factor variables.
-#' @param df the dataframe that we would like to impute NAs.
-#' @param df_true optional complete dataframe of the dataframe provided as df argument
-#' @param factors a character vector of factor variables that we would like specfiy as factors when imputing
-#' These factors variables can be numeric or character variables in the df provided
+#' @param data the dataframe that we would like to impute NAs.
+#' @param data_true optional complete dataframe of the dataframe provided as data argument
+#' @param factors a character vector of factor variables that we would like specfiy as factors when imputing.
+#' These factors variables can be numeric or character variables in the data provided. It is recommended
+#' that does not have numeric meaning be included as a factor.
 #' @param drop a character vector of variables that will not be included in the imputation.
-#' These variables will be added to the outputted dataframe, even though they are not inclued in the imputation.
+#' These variables will be added to the outputted dataframe, even though they are not included in the imputation.
+#' Variables to include here would be dates as missForest cannot impute dates.
 #' @param ntree number of trees to grow in each forest. Default is set to 500.
 #' @return A list of output similar to missForest
 #'     \item{ximp}{
-#'     a dataframe with no missing values. The resulting dataframe will be of the same size as the original df
+#'     a dataframe with no missing values. The resulting dataframe will be of the same size as the original data
 #'     provided, but with all of the NA's imputed. However, if there are NA's in the drop columns, these values wil
 #'     not be imputed.
 #'     }
@@ -36,19 +38,19 @@
 #' @export
 
 
-impute_mf <- function(df, df_true = NULL, factors = NULL, drop = NULL, ntree = 500, ...){
-  df_drop <- df[,drop] # save drop variables
+impute_mf <- function(data, data_true = NULL, factors = NULL, drop = NULL, ntree = 500, ...){
+  data_drop <- data[,drop] # save drop variables
   # convert to be factor variables to character, then to factor. This insures no excess factor levels if the variable is originally numeric
-  df[,factors] <- lapply(factors, function(factor)as.factor(as.character(df[,factor])))
+  data[,factors] <- lapply(factors, function(factor)as.factor(as.character(data[,factor])))
 
-  dm_prep <- df[!(names(df) %in% drop)] # drop drop variables
+  dm_prep <- data[!(names(data) %in% drop)] # drop drop variables
   dm_prep <- data.matrix(dm_prep) # create data matrix
 
-  if(!is.null(df_true)){
+  if(!is.null(data_true)){
     # convert to be factor variables to character, then to factor. This insures no excess factor levels if the variable is originally numeric
-    df_true[,factors] <- lapply(factors, function(factor)as.factor(as.character(df_true[,factor])))
+    data_true[,factors] <- lapply(factors, function(factor)as.factor(as.character(data_true[,factor])))
 
-    dm_true_prep <- df_true[!(names(df_true) %in% drop)] # drop drop variables
+    dm_true_prep <- data_true[!(names(data_true) %in% drop)] # drop drop variables
     dm_true_prep <- data.matrix(dm_true_prep) # create data matrix
   }
 
@@ -57,9 +59,9 @@ impute_mf <- function(df, df_true = NULL, factors = NULL, drop = NULL, ntree = 5
   imp <- missForest::missForest(dm_prep, ntree = ntree, xtrue=dm_true_prep, ...) # impute with missed forest
 
   imp$ximp <- as.data.frame(imp$ximp)
-  imp$ximp <- cbind(imp$ximp, df_drop) # bind imputed dataframe with dropped columns
+  imp$ximp <- cbind(imp$ximp, data_drop) # bind imputed dataframe with dropped columns
   imp$ximp[,factors] <- lapply(factors, function(fact){ # use old factor levels to relabel factor variables
-    factor(as.character(round(imp$ximp[,fact])), labels = levels(df[,fact]))
+    factor(as.character(round(imp$ximp[,fact])), labels = levels(data[,fact]))
   })
 
   imp$ximp <- imp$ximp
