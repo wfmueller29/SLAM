64251402ded1c59a26c55d9fc287822fa6319889
William Mueller <wfmueller29@gmail.com>
2022-02-16 22:18:15 -0500

    improved surv_example

--- R/surv_gethr.R
+++ R/surv_gethr.R
@@ -28,60 +28,83 @@
 #'  }
 #'
 #' @examples
-#' ### Repeated Measures (Longitudinal) Example
-#' ## Lets see how glucose predicts mortaility in SLAM
+#' # Repeated Measures (Longitudinal) Example
+#' # Lets see how glucose predicts mortaility in SLAM
 #'
-#' library(dplyr)
-#' library(SLAM)
+#' if (requireNamespace("dplyr", quietly = TRUE)) {
+#'   # Checkout dataframes --------------------------------------------------------
+#'   # Checkout census
+#'   head(data_SLAM_census)
 #'
-#' ## Checkout census
-#' head(data_SLAM_census)
+#'   # Checkout glucose
+#'   head(data_SLAM_gluc)
 #'
-#' ## Checkout glucose
-#' head(data_SLAM_gluc)
+#'   # Checkout survival data
+#'   head(data_SLAM_surv)
 #'
-#' ## Checkout survival data
-#' head(data_SLAM_surv)
+#'   # Create dataframe with everything -------------------------------------------
+#'   # drop lactate to simplify
+#'   main <- dplyr::select(data_SLAM_gluc, -lact)
+#'   # obtain census info for dob
+#'   main <- dplyr::left_join(main, data_SLAM_census, by = "idno")
+#'   # obtain survival info for dod
+#'   main <- dplyr::left_join(main, data_SLAM_surv, by = "tag")
+#'   # filter mice without date of death
+#'   main <- dplyr::filter(main, !is.na(died))
+#'   # create age, age of death, and difference between age and age of death
+#'   main <- dplyr::mutate(main, age_wk = as.numeric(difftime(date, dob, units = "weeks")),
+#'                         age_wk_death = as.numeric(difftime(died, dob, units = "weeks")),
+#'                         dif = age_wk_death - age_wk)
+#'   # filter mice measured after death because tmerge will throw error
+#'   main <- dplyr::filter(main, age_wk <= age_wk_death)
+#'   # filter mice that were measured same day as death because tmerge with throw an error
+#'   main <- dplyr::filter(main, !(age_wk == age_wk_death))
 #'
-#' ## Create dataframe with everything
-#' main <- data_SLAM_gluc %>%
-#'   select(-lact) %>% ## drop lactate
-#'   left_join(data_SLAM_census, by = "idno") %>% ## merge with census
-#'   left_join(data_SLAM_surv, by = "tag") %>% ## merge with survival
-#'   filter(!is.na(died)) %>% ## filter mice that have not died
-#'   mutate(age_wk = as.numeric(difftime(date, dob, units = "weeks")),  ## create age from date and dob
-#'          age_wk_death = as.numeric(difftime(died, dob, units = "weeks")), ## create age_wk_death from dob and died
-#'          dif = age_wk_death - age_wk) %>%## Create dif, the time between when the mouse was measured and it died
-#'   filter(age_wk <= age_wk_death) %>%## filter mice that were measured after their death
-#'   filter(!(age_wk == age_wk_death)) ## filter mice that were measured same day as death
+#'   # Checkout main --------------------------------------------------------------
+#'   # Table death censor. 0 means death was not natural and 1 means natural deat
+#'   table(main$dead_censor)
 #'
-#' ## Table death censor. 0 means death was not natural and 1 means natural deat
-#' table(main$dead_censor)
+#'   # Checkout main
+#'   head(main)
+#'   # Checkout main NA's
+#'   apply(apply(main,2,is.na),2,sum)
 #'
-#' ## Checkout main
-#' head(main)
-#' ## Checkout main NA's
-#' apply(apply(main,2,is.na),2,sum)
+#'   # Now use surv_tmerge --------------------------------------------------------
+#'   main_tmerge <- surv_tmerge(data = main,
+#'                              id = "idno",
+#'                              age = "age_wk",
+#'                              age_death = "age_wk_death",
+#'                              death_censor = "dead_censor",
+#'                              outcomes = c("gluc"))
 #'
-#' ### Now use surv_tmerge
-#' main_tmerge <- surv_tmerge(data = main, id = "idno", age = "age_wk", age_death = "age_wk_death", death_censor = "dead_censor", outcomes = c("gluc"))
+#'   # Now lets make a cox model with our now time dependent dataframe ------------
+#'   fit <- surv_cox(data = main_tmerge,
+#'                   covariates = ~gluc+age_wk+sex+strain,
+#'                   time = "tstart",
+#'                   time2 = "tstop",
+#'                   death = "death")
 #'
-#' ### Now lets make a cox model with our now time dependent dataframe
-#' fit <- surv_cox(data = main_tmerge, covariates = ~gluc+tt(age_wk)+sex+strain, time = "tstart", time2 = "tstop", death = "death", tt = function(x,t,...)x*log(t+20))
+#'   # Now lets extract Hazard Ratios ------------------------------------------
+#'   hrs <- surv_gethr(fit = fit,
+#'                     vars =c("gluc", "age_wk"),
+#'                     names = c("Glucose", "Age (weeks)"),
+#'                     ndec = 4)
 #'
-#' ### Now lets extract Hazard Ratios for glucose and age
-#' hrs <- surv_gethr(fit, c("gluc", "tt(age_wk)"), names = c("Glucose", "Age (weeks)"), ndec = 4)
+#'   # Lets look at final HR table
+#'   dplyr::select(hrs$hr_table, final)
 #'
-#' ## Lets look at final HR table
-#' hrs$hr_table %>%
-#'   select(final)
+#'   # Lets make predictions on other data ---------------------------------------
+#'   # create new data for 4 mice
+#'   pred_df <- data.frame(age_wk = c(40, 80, 20, 100),
+#'                         gluc = c(180, 200, 150, 120),
+#'                         sex = c("M", "M", "F","F"),
+#'                         strain = c("B6", "HET3","B6","HET3"))
+#'   # use predict function to get HR for each mouse
+#'   predict(fit, newdata = pred_df, type = "risk")
 #'
-#' ## Lets make predictions on other data
-#' pred_df <- data.frame(age_wk = c(40, 80, 20, 100),
-#'                       gluc = c(180, 200, 150, 120),
-#'                       sex = c("M", "M", "F","F"),
-#'                       strain = c("B6", "HET3","B6","HET3"))
-#' predict(fit, newdata = pred_df, type = "risk")
+#' } else{
+#'   print("Install dplyr to run this example")
+#' }
 #'
 #' @export
 
