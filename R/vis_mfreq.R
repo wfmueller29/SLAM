00dfc32655ba85b52993ab1564cadac72fe943d1
Mueller <muellerwf@nih.gov>
2022-03-08 12:10:02 -0500

    vis_mfreq added

---
+++ R/vis_mfreq.R
@@ -0,0 +1,27 @@
+#' Visualize Measurement Frequency
+#'
+#' Creates a barplot of measurement frequencies by some id column
+#'
+#' @param data a dataframe in long format
+#' @param id a character string specifying subject id
+#' @param ... optional parameters to pass to barplot function
+#'
+#' @return
+#' prints a barplot
+#'
+#' @seealso \link[graphics]{barplot}
+#'
+#' @author William Mueller
+#'
+#' @export
+#'
+
+
+vis_mfreq <- function(data, id, ...){
+  data <- data.table::as.data.table(data)
+  j <- quote(.N)
+  by <- id
+  cl <- quote(data[, j = eval(j), by = eval(by)][,N])
+  count <- eval(cl)
+  barplot(table(count), xlab = "Observations per idno", ...)
+}
