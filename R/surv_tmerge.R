2bc9007d7b1f5d601ee9857f6899354aa7cc72f0
William Mueller <williammueller@williams-mcp.mynetworksettings.com>
2025-02-23 20:43:19 -0500

    Fixed bug in surv_tmerge where dead_censor applied to all columns

--- R/surv_tmerge.R
+++ R/surv_tmerge.R
@@ -37,35 +37,31 @@ surv_tmerge <- function(data, id, age, age_death, dead_censor, outcomes) {
 
   # First tmerge ---------------------------------------------------------------
   # Create call for first tmerge
-  event <- call("event", as.symbol(age_death), as.symbol(dead_censor))
   cl_tmerge1 <- rlang::call2("tmerge",
     data1 = as.symbol("data_baseline"),
     data2 = as.symbol("data_baseline"),
     id = as.symbol(id),
     tstart = as.symbol(age),
     tstop = as.symbol(age_death),
-    death = event,
     .ns = "survival"
   )
 
-  # Call first tmerge, this will create a tstart, tstop, and death column
+  # Call first tmerge, this will create a tstart, tstop
   # tstart is the time of first observation
   # tstop is the time of last observation
-  # death is weather or not the individual died following the time of last
-  # observation
   data1 <- eval(cl_tmerge1)
 
   # Second tmerge --------------------------------------------------------------
   # Create call for second tmerge,
-  # This will fill in all the times between the first and last observation for
-  # each subject and make death column 0
   # Create tmerge ... arguments and store them as args. age is included so it
   # will be constant
   args <- lapply(c(outcomes, age), function(outcome) {
     call("tdc", as.symbol(age), as.symbol(outcome))
   })
+  event <- call("event", as.symbol(age_death), as.symbol(dead_censor))
+  args <- c(args, event)
   # name args
-  names(args) <- c(outcomes, age)
+  names(args) <- c(outcomes, age, dead_censor)
   # Create call
   cl_tmerge2 <- rlang::call2("tmerge",
     data1 = as.symbol("data1"),
